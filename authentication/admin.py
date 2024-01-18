fc61a90b0216fd8b98062cf98b7036c7998ea8cd
unknown <justinhmosher@gmail.com>
2024-01-17 18:40:41 -0800

    added week model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Pick,Paid,NFLPlayer
+from .models import Pick,Paid,NFLPlayer,Week
 
 # Register your models here.
 
 admin.site.register(Pick)
 admin.site.register(Paid)
 admin.site.register(NFLPlayer)
+admin.site.register(Week)
--- authentication/models.py
+++ authentication/models.py
@@ -1,27 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-"""
-class Player(models.Model):
-    team = models.CharField(max_length=100)
-    position = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.name} - {self.team} ({self.position})"
-
-class SavedPlayer(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    player_id = models.IntegerField() # Adjust the max length as needed
-
-    def __str__(self):
-        return f"{self.user.username}'s pick: {self.player_name}"
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Add any other profile-related fields here
-"""
-
 class Pick(models.Model):
     your_primary_key = models.AutoField(primary_key=True)
     team_name = models.CharField(max_length = 100, default='Default Team Name')
@@ -48,3 +27,8 @@ class NFLPlayer(models.Model):
     player_ID = models.CharField(max_length= 100, default = "player ID")
     def __str__(self):
         return f"{self.name}"
+
+class Week(models.Model):
+    week = models.IntegerField(default = 1)
+    def __str__(self):
+        return "week"
