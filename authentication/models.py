f2085102f69b953b42b158c4803098cc8745101d
unknown <justinhmosher@gmail.com>
2024-01-23 20:43:34 -0800

    added image field for helmets

--- authentication/models.py
+++ authentication/models.py
@@ -26,6 +26,7 @@ class NFLPlayer(models.Model):
     position = models.CharField(max_length= 100, default = "player position name")
     team_name = models.CharField(max_length= 100, default = "player team name")
     player_ID = models.CharField(max_length= 100, default = "player ID")
+    image = models.ImageField(null = True, blank = True, upload_to='images/')
     def __str__(self):
         return f"{self.name}"
 
