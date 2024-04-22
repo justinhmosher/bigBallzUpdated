4c42f16f4e23328e2a13ef12f751d011c8346257
unknown <justinhmosher@gmail.com>
2024-04-21 18:07:11 -0700

    polishing

--- authentication/views.py
+++ authentication/views.py
@@ -649,7 +649,10 @@ def checking(request):
 	elif paid.paid_status == False:
 		return redirect('payment')
 	elif (paid.paid_status == True) and not (start_date <= current_day < end_date):
-		#return render(request,'authentication/waiting.html',{'start_date':start_date})
+		username = request.user.username
+		current_day = datetime.now().weekday()
+		if not Pick.objects.filter(username = username).exists():
+			return redirect('teamname')
 		return redirect('game')
 	else:
 		username = request.user.username
