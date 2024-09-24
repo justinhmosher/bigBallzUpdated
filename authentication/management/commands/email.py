b7daae1b9161e3d79fcc9fbcd6ced77373eb8e4e
unknown <justinhmosher@gmail.com>
2024-09-24 16:18:57 -0700

    email

--- authentication/management/commands/email.py
+++ authentication/management/commands/email.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
             message = MIMEMultipart()
             message['From'] = f"{sender_name} <{sender_email}>"
             message['To'] = receiver_email
-            message['Subject'] = "Week 3 Pick Reminder"
+            message['Subject'] = "Week 3 Recap & Picks Reminder"
             body = render_to_string('authentication/emarketing.html')
             message.attach(MIMEText(body, "html"))
             text = message.as_string()
