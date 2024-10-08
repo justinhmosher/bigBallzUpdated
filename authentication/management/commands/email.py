9cdcb63301642ee285cdff7e9417aa8434919518
unknown <justinhmosher@gmail.com>
2024-10-08 16:34:00 -0700

    polishing

--- authentication/management/commands/email.py
+++ authentication/management/commands/email.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
             message = MIMEMultipart()
             message['From'] = f"{sender_name} <{sender_email}>"
             message['To'] = receiver_email
-            message['Subject'] = "Week 5 Picks Are Out"
+            message['Subject'] = "Top 15 Teams!"
             body = render_to_string('authentication/emarketing.html')
             message.attach(MIMEText(body, "html"))
             text = message.as_string()
