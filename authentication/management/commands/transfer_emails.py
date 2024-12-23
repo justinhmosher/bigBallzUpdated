3a64574110ba261c0d1b0051f782c6800022cc94
unknown <justinhmosher@gmail.com>
2024-12-23 09:23:02 -0800

    transfer emails to new data bases

--- authentication/management/commands/transfer_emails.py
+++ authentication/management/commands/transfer_emails.py
@@ -1,14 +1,26 @@
 from django.core.management.base import BaseCommand
 from authentication.models import Paid, Email
+from authentication.NFL_weekly_view.models import PaidNW,PromoUserNW
 
 class Command(BaseCommand):
     help = 'Transfer all emails from Paid to Email'
 
     def handle(self, *args, **kwargs):
         Email.objects.all().delete()
+        PaidNW.objects.all().delete()
+        PromoUserNW.objects.all().delete()
         paid_list = Paid.objects.all()
         for paid in paid_list:
             new_email = Email(
                 email = paid.username
                 )
             new_email.save()
+            new_paid = PaidNW(
+                username = paid.username
+                )
+            new_paid.save()
+            new_promo = PromoUserNW(
+                username = paid.username
+                )
+            new_promo.save()
+
