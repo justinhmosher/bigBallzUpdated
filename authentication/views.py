a720371000a620382105acce57dad431389d9c72
unknown <justinhmosher@gmail.com>
2025-02-05 14:30:58 -0800

    polishing

--- authentication/views.py
+++ authentication/views.py
@@ -20,8 +20,8 @@ from django.http import JsonResponse
 from .forms import PlayerSearchForm, Pickform, Pick1Form, CreateTeam
 from .models import Pick,Scorer,Paid,NFLPlayer,Game,PastPick,PromoCode,PromoUser,OfAge,UserVerification,Blog,ChatMessage,Waitlist,MessageReaction,Message,Email,KYC
 from authentication.NFL_weekly_view.models import PickNW
-from authentication.baseball_SL.models import PickBL
-from authentication.baseball_WL.models import PickBS
+from authentication.baseball_SL.models import PickBL,PromoUserBL
+from authentication.baseball_WL.models import PickBS,PromoUserBS
 from django.db.models import Count,F,ExpressionWrapper,fields,OuterRef,Subquery
 from datetime import datetime, time
 from itertools import chain
@@ -419,6 +419,14 @@ def activate(request, uidb64, token):
 			NW_paid = PaidNW(username = myuser.username)
 			NW_paid.save()
 			NW_promo = PromoUserNW(username = myuser.username)
+			NW_promo.save()
+			NW_paid = PaidBL(username = myuser.username)
+			NW_paid.save()
+			NW_promo = PromoUserBL(username = myuser.username)
+			NW_promo.save()
+			NW_paid = PaidBS(username = myuser.username)
+			NW_paid.save()
+			NW_promo = PromoUserBS(username = myuser.username)
 			NW_promo.save()
 			email = Email(email = myuser.username)
 			email.save()
